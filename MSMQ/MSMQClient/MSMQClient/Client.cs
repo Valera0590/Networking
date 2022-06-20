d6752a60614b735aa99c50cd48643cb3a4dd4e6a
Valeriy <84934536+Valera0590@users.noreply.github.com>
2022-06-21 01:18:26 +0500

    Добавлен проект с MSMQ
    Добавлен проект с взаимодействием клиент-сервер с использование очередей сообщения MSMQ.

---
+++ MSMQ/MSMQClient/MSMQClient/Client.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Messaging;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MSMQ
+{
+    public partial class frmMain : Form
+    {
+        private MessageQueue q = null;      // очередь сообщений, в которую будет производиться запись сообщений
+        private bool _continue = true;          // флаг, указывающий продолжается ли работа с мэйлслотом
+        private MessageQueue qCl = null;      // очередь сообщений, в которую будет производиться запись сообщений
+
+
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+            string path = Dns.GetHostName() + "\\private$\\ClientQueue";    // путь к очереди сообщений, Dns.GetHostName() - метод, возвращающий имя текущей машины
+
+            // если очередь сообщений с указанным путем существует, то открываем ее, иначе создаем новую
+            if (MessageQueue.Exists(path))
+                qCl = new MessageQueue(path);
+            else
+                qCl = MessageQueue.Create(path);
+
+            // задаем форматтер сообщений в очереди
+            qCl.Formatter = new XmlMessageFormatter(new Type[] { typeof(String) });
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (MessageQueue.Exists(tbPath.Text))
+            {
+                // если очередь, путь к которой указан в поле tbPath существует, то открываем ее
+                q = new MessageQueue(tbPath.Text);
+                btnSend.Enabled = true;
+                btnConnect.Enabled = false;
+                tbPath.Enabled = false;
+                tbLogin.Enabled = false;
+                string message = "присоединился(-ась) к чату";
+                if (tbLogin.Text == "")
+                {
+                    // выполняем отправку сообщения в очередь
+                    q.Send(message, Dns.GetHostName());
+                    this.Text += "     " + Dns.GetHostName();
+                }
+                else
+                {
+                    q.Send(message, tbLogin.Text);
+                    this.Text += "     " + tbLogin.Text;
+                }
+                await ReceiveMessageAsync();   // вызов асинхронного метода
+            }
+            else
+                MessageBox.Show("Указан неверный путь к очереди, либо очередь не существует");
+        }
+
+        private async Task ReceiveMessageAsync()
+        {
+            await Task.Run(() => ReceiveMessage());
+        }
+
+        // получение сообщения
+        private void ReceiveMessage()
+        {
+            if (qCl == null)
+                return;
+            System.Messaging.Message msg = null;
+            
+            // входим в бесконечный цикл работы с очередью сообщений
+            while (_continue)
+            {
+                if (qCl.Peek() != null)   // если в очереди есть сообщение, выполняем его чтение, интервал до следующей попытки чтения равен 10 секундам
+                    msg = qCl.Receive(TimeSpan.FromSeconds(10.0));
+
+                rtbMessages.Invoke((MethodInvoker)delegate
+                {
+                    if (msg != null)
+                        rtbMessages.Text += "\n >> " + msg.Label + " : " + msg.Body;     // выводим полученное сообщение на форму
+                });
+
+                Thread.Sleep(500);          // приостанавливаем работу потока перед тем, как приcтупить к обслуживанию очередного клиента
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (tbLogin.Text == "")
+            {
+                // выполняем отправку сообщения в очередь
+                q.Send(tbMessage.Text, Dns.GetHostName());
+            }
+            else q.Send(tbMessage.Text, tbLogin.Text);
+            tbMessage.Text = "";
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (btnSend.Enabled == true)
+            {
+                string message = "вышел(-ла) из чата";
+                if (tbLogin.Text == "")
+                {
+                    // выполняем отправку сообщения в очередь
+                    q.Send(message, Dns.GetHostName());
+                    this.Text += "     " + Dns.GetHostName();
+                }
+                else    q.Send(message, tbLogin.Text);
+
+            }
+
+            _continue = false;      // сообщаем, что работа с очередью завершена
+        }
+
+        private void tbLogin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != Convert.ToChar(">")) return;
+            else
+                e.Handled = true;
+        }
+    }
+}
---
+++ MSMQ/MSMQClient/MSMQClient/Client.designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MSMQ
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(375, 347);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(121, 32);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(123, 352);
+            this.tbMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(240, 22);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(9, 16);
+            this.lblIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(108, 16);
+            this.lblIP.TabIndex = 2;
+            this.lblIP.Text = "Путь к очереди";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(375, 12);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 60);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Подключиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(9, 356);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(81, 16);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Сообщение";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(123, 12);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(240, 22);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.Text = ".\\private$\\ServerQueue";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Логин/ник";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(123, 50);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(240, 22);
+            this.tbLogin.TabIndex = 5;
+            this.tbLogin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbLogin_KeyPress);
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(12, 101);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.Size = new System.Drawing.Size(484, 239);
+            this.rtbMessages.TabIndex = 6;
+            this.rtbMessages.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Чат";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 396);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbMessages);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Клиент";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
