d7e6402320826006577bb7758ca33bddeded4f8e
Valeriy <84934536+Valera0590@users.noreply.github.com>
2022-06-21 01:15:14 +0500

    Добавлен проект с UDP Sockets

---
+++ Sockets(UDP)/SocketsClient/SocketsClient/Client.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sockets
+{
+    public partial class frmMain : Form
+    {
+        private IPAddress ipAddr;
+        private Random rnd = new Random(DateTime.Now.Millisecond);
+        private IPAddress IP;
+        private int PortClient;
+        private bool _continue = true;                          // флаг, указывающий продолжается ли работа с сокетами
+
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());    // информация об IP-адресах и имени машины, на которой запущено приложение
+            IP = hostEntry.AddressList[0];                                  // IP-адрес, который будет указан в заголовке окна для идентификации клиента
+            // определяем IP-адрес машины в формате IPv4
+            foreach (IPAddress address in hostEntry.AddressList)
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = address;
+                    break;
+                }
+
+            this.Text += "     " + IP.ToString();                           // выводим IP-адрес текущей машины в заголовок формы
+
+        }
+
+        // подключение к серверному сокету
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            ipAddr = IPAddress.Parse("127.0.0.1");
+            PortClient = rnd.Next(5002, 49151);
+            try
+            {
+                // Создаем UdpClient
+                UdpClient udpClient = new UdpClient();
+                // Соединяемся с удаленным хостом
+                udpClient.Connect(ipAddr, 5001);
+
+                tbLogin.ReadOnly = true;
+                btnConnect.Enabled = false;
+                btnSend.Enabled = true;
+
+                byte[] data = new byte[1024];
+                data = Encoding.UTF8.GetBytes(PortClient.ToString() + "!" + IP.ToString() + ">>" + tbLogin.Text + ">>" + "присоединился(-ась) к чату");
+                udpClient.Send(data, data.Length);
+                
+                //Закрываем соединение
+                udpClient.Close();   
+                await ReceiveMessageAsync();   // вызов асинхронного метода
+                this.Text += "     " + tbLogin.Text;
+            }
+            catch
+            {
+                MessageBox.Show("Error!");
+                //tbIP.ReadOnly = false;
+                tbLogin.ReadOnly = false;
+                btnConnect.Enabled = true;
+                btnSend.Enabled = false;
+            }
+        }
+        private async Task ReceiveMessageAsync()
+        {
+            await Task.Run(() => ReceiveMessage());
+        }
+        // работа с серверным сокетом
+        private void ReceiveMessage()
+        {
+            byte[] data = new byte[1024];
+            string msg = "";        // полученное сообщение
+           
+            // Создаем UdpClient
+            UdpClient udpClient = new UdpClient(PortClient);
+            // входим в бесконечный цикл для работы с клиентским сокетом
+            while (_continue)
+            {
+                // Создаем переменную IPEndPoint, чтобы передать ссылку на нее в Receive()
+                IPEndPoint RemoteIPEndPoint = null;
+                data = udpClient.Receive(ref RemoteIPEndPoint);
+                msg = Encoding.UTF8.GetString(data);     // выполняем преобразование байтов в последовательность символов
+                msg = msg.Replace("\0", "");
+                if (msg != "")
+                {
+                    rtbChat.Invoke((MethodInvoker)delegate
+                    {
+                        rtbChat.Text += "\n" + msg;             // выводим полученное сообщение на форму
+                    });
+                }
+                Thread.Sleep(200);
+            }
+                // Закрываем соединение
+                udpClient.Close();
+        }
+
+        // отправка сообщения
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Создаем UdpClient
+            UdpClient udpClient = new UdpClient();
+            // Соединяемся с удаленным хостом
+            udpClient.Connect(ipAddr, 5001);
+
+            byte[] data = new byte[1024];
+            data = Encoding.UTF8.GetBytes(IP.ToString() + ">>" + tbLogin.Text + ">>" + tbMessage.Text);   // выполняем запись последовательности байт 
+            udpClient.Send(data, data.Length);
+
+            tbMessage.Text = "";
+            // Закрываем соединение
+            udpClient.Close();
+        }
+
+
+        private void tbLogin_TextChanged(object sender, EventArgs e)
+        {
+            if (/*tbIP.Text != "" && */tbLogin.Text != "")
+                btnConnect.Enabled = true;
+            else btnConnect.Enabled = false;
+        }
+
+        /*private void tbIP_TextChanged(object sender, EventArgs e)
+        {
+            if (tbIP.Text != "" && tbLogin.Text != "")
+                btnConnect.Enabled = true;
+            else btnConnect.Enabled = false;
+        }*/
+        private void tbLogin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != Convert.ToChar(">")) return;
+            else
+                e.Handled = true;
+        }
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+          5
+        }
+
+    }
+}
---
+++ Sockets(UDP)/SocketsServer/SocketsServer/Server.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Sockets
+{
+    public partial class frmMain : Form
+    {
+        private bool _continue = true;                          // флаг, указывающий продолжается ли работа с сокетами
+        private IPAddress IP;                           // IP-адрес клиента
+        private List<IPAddress> IPClients = new List<IPAddress>();
+        private List<string> LoginClients = new List<string>();
+        private List<int> PortClients = new List<int>();
+        private IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+
+        // конструктор формы
+        public frmMain()
+        {
+            InitializeComponent();
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());    // информация об IP-адресах и имени машины, на которой запущено приложение
+            IP = hostEntry.AddressList[0];                                  // IP-адрес, который будет указан в заголовке окна для идентификации клиента
+            // определяем IP-адрес машины в формате IPv4
+            foreach (IPAddress address in hostEntry.AddressList)
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = address;
+                    break;
+                }
+
+            this.Text += "     " + IP.ToString();                           // выводим IP-адрес текущей машины в заголовок формы
+
+            LoginClients.Clear();
+            PortClients.Clear();
+            IPClients.Clear();
+
+            AsnStart();
+        }
+        
+        private async void AsnStart()
+        {
+            await ReceiveMessageAsync();        //вызов асинхронного метода обработки сообщений
+        }
+
+        private async Task ReceiveMessageAsync()
+        {
+            await Task.Run( () => ReceiveMessage());
+        }
+
+        // получение сообщений от клиента
+        private void ReceiveMessage()
+        {
+            byte[] data = new byte[1024];
+            string msg = "";        // полученное сообщение
+            string[] ArrMsgs = new string[3];
+
+            // входим в бесконечный цикл для работы с клиентским сокетом
+            while (_continue)
+            {
+                // Создаем UdpClient
+                UdpClient udpClient = new UdpClient(5001);
+                // Создаем переменную IPEndPoint, чтобы передать ссылку на нее в Receive()
+                IPEndPoint RemoteIPEndPoint = null;
+                data = udpClient.Receive(ref RemoteIPEndPoint);
+                msg = Encoding.UTF8.GetString(data);     // выполняем преобразование байтов в последовательность символов
+                msg = msg.Replace("\0", "");
+                if (msg != "")
+                {
+                    ArrMsgs = SplitMsg(msg);
+                    rtbMessages.Invoke((MethodInvoker)delegate
+                    {
+                        rtbMessages.Text += "\n >>  " + ArrMsgs[1] + "  >>  " + ArrMsgs[2];             // выводим полученное сообщение на форму
+                    });
+                    Thread.Sleep(100);
+                    for (int i = 0; i < LoginClients.Count(); i++)     // отправка сообщений всем присоединившимся клиентам
+                    {
+                        UdpClient udpServer = new UdpClient();
+
+                        // Соединяемся с удаленным хостом
+                        udpServer.Connect(ipAddr, PortClients[i]);
+
+                        byte[] data_s = new byte[1024];
+                        data_s = Encoding.UTF8.GetBytes(" >>  " + ArrMsgs[1] + "  >>  " + ArrMsgs[2]);   // выполняем запись последовательности байт 
+                        udpServer.Send(data_s, data_s.Length);
+
+                        // Закрываем соединение
+                        udpServer.Close();
+                    }
+                }
+                // Закрываем соединение
+                udpClient.Close();
+                Thread.Sleep(100);
+            }
+
+        }
+        private string[] SplitMsg(string message)
+        {
+            string[] ArrayMessages = new string[3];
+            for (int i = 0; i < ArrayMessages.Count(); i++)
+            {
+                ArrayMessages[i] = "";
+            }
+            
+            int j = 0;
+            while(message != "")
+            {
+                if (message[0] == '>' && j < 2)
+                {
+                    message = message.Remove(0, 2);
+                    j++;
+                }
+                else
+                {
+                    ArrayMessages[j] += message[0];
+                    message = message.Remove(0, 1);
+                }
+            }
+            
+            if (!LoginClients.Contains(ArrayMessages[1]))               // при первом подключении клиента
+            {
+                string[] temp = ArrayMessages[0].Split('!');
+                ArrayMessages[0] = temp[1];
+                PortClients.Add(Int32.Parse(temp[0]));
+                LoginClients.Add(ArrayMessages[1]);
+                IPClients.Add(IPAddress.Parse(ArrayMessages[0]));
+            }
+            if (ArrayMessages[2] == "вышел(-ла) из чата")
+            {
+                PortClients.RemoveAt(LoginClients.IndexOf(ArrayMessages[1]));
+                LoginClients.Remove(ArrayMessages[1]);
+                IPClients.Remove(IPAddress.Parse(ArrayMessages[0]));
+            }
+            return ArrayMessages;
+        }
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _continue = false;      // сообщаем, что работа с сокетами завершена
+        }
+    }
+}
